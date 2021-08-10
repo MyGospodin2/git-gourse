99f1474481f7261a861ff154c710b8fc4dae3948
MyGospodin2 <batman0311@mail.ru>
2021-08-10 23:55:28 +0300

    changes-2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
   public int add (int a,int b) {
       return a + b;
   }
-        }
+      public void sayHello () {
+          System.out.println("Hello");
+
+      }
+          }
